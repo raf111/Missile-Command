9fdd4e37e8d82e1ec8759fdda1556688b33fc3dc
Mogin Daniloff <mogindaniloff@Mogins-MacBook-Pro.local>
2017-06-12 10:22:58 -0400

    making significant progress

--- main.js
+++ main.js
@@ -7,8 +7,13 @@ jQuery(function() {
     var land_page_removed = 0;
     var score = 0;
     var high_score = 0;
-    var level = 1;
-    var cities_intact = 6;
+    var level = 100;
+    var line_speed = 10000 / (level * 0.5) + 5000;
+    var cities = 6;
+    var nextLine = 1000 / level;
+    var counter = 0
+    var need_new_level = false;
+    var gameOver = false;
 
 
     /* Variable References */
@@ -61,8 +66,15 @@ jQuery(function() {
     var game_page = $('<div>');
     game_page.addClass('game_page');
 
+    var game_box = $('<div>');
+    game_box.addClass('game_box');
+    game_page.append(game_box);
+
     var nav_bar = $('<nav>');
     nav_bar.addClass('nav_bar');
+
+    var level_page = $('<div>');
+    level_page.addClass('level_page');
 
     var game_menu_btn = $('<button>');
     game_menu_btn.addClass('game_menu_btn');
@@ -114,23 +126,33 @@ jQuery(function() {
         var missile_id_1 = 'A' + i.toString();
         missile_1.attr('id', missile_id_1);
         silo_1.append(missile_1);
-    }
 
-    for (var i = 1; i < 11; i++) {
         var missile_2 = $('<div>');
         missile_2.addClass('missile_2');
         var missile_id_2 = 'B' + i.toString();
         missile_2.attr('id', missile_id_2);
         silo_2.append(missile_2);
-    }
 
-    for (var i = 1; i < 11; i++) {
         var missile_3 = $('<div>');
         missile_3.addClass('missile_3');
         var missile_id_3 = 'C' + i.toString();
         missile_3.attr('id', missile_id_3);
         silo_3.append(missile_3);
     }
+
+    for (var i = 1; i < 10; i++) {
+        var line = $('<div>');
+        line.addClass('line');
+        var line_id = 'line' + i.toString();
+        line.attr('id', line_id);
+        var position = (i * 100) - 60;
+        var right_move = 'translateX(' + position.toString() + 'px)';
+        line.css({
+            transform: right_move
+        })
+        game_box.append(line);
+    }
+
 
     var menu_box = $('<div>');
     menu_box.addClass('menu_box');
@@ -143,6 +165,10 @@ jQuery(function() {
         (menu_box).append(ingame_box_menu_btn);
     }
 
+    var game_over_page = $('<div>');
+    game_over_page.addClass('game_over_page');
+    game_over_page.text('GAME OVER!!!');
+
 
     /* Function Implementations */
     var appender = function() {
@@ -153,6 +179,20 @@ jQuery(function() {
         score_box.append(game_info);
         game_page.append(nav_bar);
         game_page.append(ground);
+    }
+
+    var checkForClearance = function() {
+        var checkCounter = setInterval(function() {
+            if (cities == 0) {
+                gameOver = true;
+                console.log('Game Over');
+                console.log(gameOver);
+            } else if (counter == 3) {
+                need_new_level = true;
+                console.log(counter);
+                console.log('Meed New Level');
+            }
+        })
     }
 
     var goBack = function() {
@@ -197,16 +237,31 @@ jQuery(function() {
     }
 
     var getScorePage = function() {
+        getHighScore();
         body.append(score_page);
         score_title.text('Your score: ' + score + '!');
-        game_info.text('Level: ' + level + '\n \n Cities Intact: ' + cities_intact + '\n \n Your High Score: ' + high_score);
+        game_info.text('Level: ' + level + '\n \n Cities Intact: ' + cities + '\n \n Your High Score: ' + high_score);
         setTimeout(function() {
             removeScorePage();
             createGame();
         }, 1000)
     }
 
-
+    var sayGameOver = function() {
+        setInterval(function() {
+            if (gameOver == true) {
+                game_page.detach();
+                body.append(game_over_page);
+                setInterval(function() {
+                	game_over_page.detach();
+                }, 3000)
+                getHighScore();/*
+                body.append(score_page);
+                score_title.text('Your score: ' + score + '!');
+                game_info.text('Level: ' + level + '\n \n Cities Intact: ' + cities + '\n \n Your High Score: ' + high_score);
+           */ }
+        })
+    }
 
     var removeScorePage = function() {
         score_page.detach();
@@ -234,7 +289,7 @@ jQuery(function() {
 
     var bringInGameMenu = function() {
         $(game_menu_btn).click(function() {
-            game_page.append(menu_box);
+            game_box.append(menu_box);
             console.log('Menu');
             $('#5').text('RESUME');
             $('#6').text('RESTART');
@@ -346,19 +401,236 @@ jQuery(function() {
         gun_three_glow();
     }
 
+    var burnCity = function() {
+        var attack_one = setInterval(function() {
+            var line_1_height = $('#line1').height();
+            //console.log(line_1_height);
+            if (line_1_height > 515) {
+                silo_1.css({
+                        backgroundImage: 'url(images/Fire-PNG-image-1.png)'
+                    })
+                    //$('#line1').stop();
+                clearInterval(attack_one);
+            }
+        })
+
+        var attack_two = setInterval(function() {
+            var line_2_height = $('#line2').height();
+            //console.log(line_2_height);
+            if (line_2_height > 515) {
+                $('#city_1').css({
+                    backgroundImage: 'url(images/Fire-PNG-image-1.png)'
+                })
+                clearInterval(attack_two);
+                cities--;
+                counter++;
+            }
+        })
+
+        var attack_three = setInterval(function() {
+            var line_3_height = $('#line3').height();
+            //console.log(line_3_height);
+            if (line_3_height > 515) {
+                $('#city_2').css({
+                    backgroundImage: 'url(images/Fire-PNG-image-1.png)'
+                })
+                clearInterval(attack_three);
+                cities--;
+                counter++;
+            }
+        })
+
+
+        var attack_four = setInterval(function() {
+            var line_4_height = $('#line4').height();
+            //console.log(line_4_height);
+            if (line_4_height > 515) {
+                $('#city_3').css({
+                    backgroundImage: 'url(images/Fire-PNG-image-1.png)'
+                })
+                clearInterval(attack_four);
+                cities--;
+                counter++;
+            }
+        })
+
+        var attack_five = setInterval(function() {
+            var line_5_height = $('#line5').height();
+            //console.log(line_5_height);
+            if (line_5_height > 515) {
+                silo_2.css({
+                    backgroundImage: 'url(images/Fire-PNG-image-1.png)'
+                })
+                clearInterval(attack_five);
+            }
+        })
+
+        var attack_six = setInterval(function() {
+            var line_6_height = $('#line6').height();
+            //console.log(line_6_height);
+            if (line_6_height > 515) {
+                $('#city_4').css({
+                    backgroundImage: 'url(images/Fire-PNG-image-1.png)'
+                })
+                clearInterval(attack_six);
+                cities--;
+                counter++;
+            }
+        })
+
+        var attack_seven = setInterval(function() {
+            var line_7_height = $('#line7').height();
+            //console.log(line_7_height);
+            if (line_7_height > 515) {
+                $('#city_5').css({
+                    backgroundImage: 'url(images/Fire-PNG-image-1.png)'
+                })
+                clearInterval(attack_seven);
+                cities--;
+                counter++;
+            }
+        })
+
+        var attack_eight = setInterval(function() {
+            var line_8_height = $('#line8').height();
+            //console.log(line_8_height);
+            if (line_8_height > 515) {
+                $('#city_6').css({
+                    backgroundImage: 'url(images/Fire-PNG-image-1.png)'
+                })
+                clearInterval(attack_eight);
+                cities--;
+                counter++;
+            }
+        })
+
+        var attack_nine = setInterval(function() {
+            var line_9_height = $('#line9').height();
+            //console.log(line_9_height);
+            if (line_9_height > 515) {
+                silo_3.css({
+                    backgroundImage: 'url(images/Fire-PNG-image-1.png)'
+                })
+                clearInterval(attack_nine);
+            }
+        })
+    }
+
+    var clearLaunchers = function() {
+        var clearence_checker = setInterval(function() {
+            if (need_new_level == true) {
+                console.log('clearGame Works');
+                $(silo_1).css({ backgroundImage: 'url(images/red_launcher.png)' });
+                $(silo_2).css({ backgroundImage: 'url(images/red_launcher.png)' });
+                $(silo_3).css({ backgroundImage: 'url(images/red_launcher.png)' });
+                for (var i = 1; i < 10; i++) {
+                    var line_id = 'line' + i.toString();
+                    $('#' + line_id).remove();
+                    var line = $('<div>');
+                    line.addClass('line');
+                    line.attr('id', line_id);
+                    var position = (i * 100) - 60;
+                    var right_move = 'translateX(' + position.toString() + 'px)';
+                    line.css({
+                        transform: right_move
+                    })
+                    game_box.append(line);
+                }
+
+                var nextTimeOut = false;
+                level++;
+                var detacher = setTimeout(function() {
+                    game_page.detach();
+                    nextTimeOut = true;
+                    level_page.text('LEVEL ' + level);
+                    body.append(level_page);
+                    var appenderr = setTimeout(function() {
+                        game_page.detach();
+                        level_page.detach();
+                        body.append(game_page);
+                    }, 3000)
+                }, 1000)
+
+                counter = 0;
+                need_new_level = false;
+            }
+        })
+    }
+
+    var enemyFire = function() {
+        $(game_box).click(function() {
+            var line_wait = nextLine * 8;
+            setTimeout(function() {
+                $('#line1').animate({
+                    height: "1030px"
+                }, line_speed)
+            }, line_wait)
+
+            var line_wait = nextLine * 6;
+            setTimeout(function() {
+                $('#line2').animate({
+                    height: "1030px"
+                }, line_speed)
+            }, line_wait)
+
+            var line_wait = nextLine * 7;
+            setTimeout(function() {
+                $('#line3').animate({
+                    height: "1030px"
+                }, line_speed)
+            }, line_wait)
+
+            setTimeout(function() {
+                $('#line4').animate({
+                    height: "1030px"
+                }, line_speed)
+            }, nextLine)
+
+            var line_wait = nextLine * 4;
+            setTimeout(function() {
+                $('#line5').animate({
+                    height: "1030px"
+                }, line_speed)
+            }, line_wait)
+
+            setTimeout(function() {
+                $('#line6').animate({
+                    height: "1030px"
+                }, line_speed)
+            }, 0)
+
+            var line_wait = nextLine * 9;
+            setTimeout(function() {
+                $('#line7').animate({
+                    height: "1030px"
+                }, line_speed)
+            }, line_wait)
+
+            var line_wait = nextLine * 2;
+            setTimeout(function() {
+                $('#line8').animate({
+                    height: "1030px"
+                }, line_speed)
+            }, line_wait)
+
+            var line_wait = nextLine * 3;
+            setTimeout(function() {
+                $('#line9').animate({
+                    height: "1030px"
+                }, line_speed)
+            }, line_wait)
+        });
+    }
+
     /* Function Calls */
     appender();
     getLandingPage();
+    checkForClearance();
     getToGame();
     glowGun();
     bringInGameMenu();
-
-    /*
-    $(body).click(function() {
-    	$(body).toggle('explode');
-    })
-    */
-
-
-
+    enemyFire();
+    burnCity();
+    clearLaunchers();
+    sayGameOver();
 });
